a9c5885fe3ccbcc057802631677d25262623fd15
Wyatt Entrekin <wmentrekin@gmail.com>
2025-10-16 16:08:01 -0400

    test

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -240,10 +240,10 @@ table_style = """
 <style>
 table {border-collapse: collapse; width: 100%;}
 th, td {text-align: left; padding: 8px;}
-th, td {background-color: #f8f9fa;}
+th {background-color: #fbfbfb;}
 </style>
 """
-
+# tr:nth-child(even){background-color: #fbfbfb;}
 # Render table as HTML
 html_table = table_style + disp_df.to_html(escape=False, index=False)
 st.markdown(html_table, unsafe_allow_html=True)
