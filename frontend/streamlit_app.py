399a6635e3468b5f153d21fd467541f55dbc77ea
Wyatt Entrekin <wmentrekin@gmail.com>
2025-10-16 16:08:13 -0400

    test

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -240,7 +240,7 @@ table_style = """
 <style>
 table {border-collapse: collapse; width: 100%;}
 th, td {text-align: left; padding: 8px;}
-th {background-color: #fbfbfb;}
+th, td {background-color: #fbfbfb;}
 </style>
 """
 # tr:nth-child(even){background-color: #fbfbfb;}
