dda5d5e47656e125bed57250dc10197e577665b8
Wyatt Entrekin <wmentrekin@gmail.com>
2025-10-21 23:25:17 -0400

    fix constraints

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -350,8 +350,8 @@ with st.expander("⚖️ Constraints", expanded=False):
     **Rating bounds:**
     $$
     \\begin{aligned}
-    r_i &\\geq 0 \\forall i \\in \\mathcal{T} \\\\
-    & R_{\\text{min}} &\\leq r_{\\text{fcs}} \\leq R_{\\text{max}}
+    r_i &\\geq 0 & \\forall i \\in \\mathcal{T} \\\\
+    & R_{\\text{min}} \\leq r_{\\text{fcs}} \\leq R_{\\text{max}}
     \\end{aligned}
     $$
     """)
