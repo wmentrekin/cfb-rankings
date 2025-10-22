a5bd4697f19b6abdd27948a6cbad2cdb6c1cf7c9
Wyatt Entrekin <wmentrekin@gmail.com>
2025-10-22 11:26:59 -0400

    test loss penalty

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -393,7 +393,8 @@ with st.expander("📐 Objective Function", expanded=False):
     st.markdown("""
     $$
     \\begin{aligned}
-    \\text{minimize} \\quad & \\sum_{(i,j,k) \\in \\mathcal{G}} \\gamma \\cdot [\\max(0, r_{\\text{loser}} + (\\text{margin}_{i,j,k} \\cdot \\alpha_{i,j,k}) - r_{\\text{winner}})]^2 & [\\text{Soft margin penalty}] \\\\
+    \\text{minimize} \\quad & \\sum_{(i,j,k) \\in \\mathcal{G}} \\gamma_{\\text{margin}} \\cdot [\\max(0, r_{\\text{loser}} + (\\text{margin}_{i,j,k} \\cdot \\alpha_{i,j,k}) - r_{\\text{winner}})]^2 & [\\text{Margin Penalty}] \\\\
+    & + \\sum_{(i,j,k) \\in \\mathcal{F}} \\gamma_{\\text{fcs}} \\cdot [\\max(0, r_{\\text{i}} + (\\text{margin}_{i,j,k} \\cdot \\alpha_{i,j,k}) - r_{\\text{fcs}})]^2 & [\\text{FCS Margin Penalty}] \\\\
     & + \\sum_{i \\in \\mathcal{F}} \\beta \\cdot z_{\\text{fcs},i} & [\\text{FCS loss penalty}] \\\\
     & + \\sum_{i \\in \\mathcal{T}} \\lambda \\cdot (r_i - \\text{prior}_i)^2 & [\\text{Prior regularization}]
     \\end{aligned}
