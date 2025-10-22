11a1b4262a67c0026c50aa2e43c23583f8724636
Wyatt Entrekin <wmentrekin@gmail.com>
2025-10-22 10:47:38 -0400

    test loss penalty

--- model/model.py
+++ model/model.py
@@ -54,7 +54,7 @@ def get_ratings(year, week = None):
     R_max = 15 # maximum FCS rating
     gamma_margin = 0.01 # small regularization constant
     gamma_loss = 0.2 # regularitzation constant for loss rate
-    nu = 500000 # large regularization constant
+    nu = 50000 # large regularization constant
 
     # Decision Variables
     r = {team: cp.Variable(name = f"r_{team}") for team in teams} # team rating
